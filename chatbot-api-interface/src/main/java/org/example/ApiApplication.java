afe1e1962e80dfb73728c2bd406921026cfe51a3
symphony <2362534639@qq.com>
2024-06-05 09:42:25 +0800

    feat: 骨架搭建

---
+++ chatbot-api-interface/src/main/java/org/example/ApiApplication.java
@@ -0,0 +1,18 @@
+package org.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @description: 启动类
+ * @author: symphony
+ * @create: 2024/06/05
+ **/
+@SpringBootApplication
+public class ApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiApplication.class, args);
+    }
+
+}
